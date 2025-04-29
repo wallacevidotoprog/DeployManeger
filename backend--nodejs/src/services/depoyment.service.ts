73c3500ffa7d02b54341e0a95fd0a3e24f7fe9b1
WallaceVidoto <wallacevidoto.tech@gmail.com>
2025-04-29 19:03:01 -0300

    modified:   .gitignore 	modified:   backend--nodejs/src/services/depoyment.service.ts

--- backend--nodejs/src/services/depoyment.service.ts
+++ backend--nodejs/src/services/depoyment.service.ts
@@ -1,33 +1,34 @@
 import { DepoymentModel } from "../models/depoyment/depoyment.model";
 import { DepoymentDtoCreate, DepoymentDtoUpdate, DepoymentDtoFindQuery } from "../models/depoyment/depoyment.dto";
+import { Request, Response } from "express";
 
 class DepoymentService {
-  static async create(req, res) {
+  static async create(req: Request, res: Response): Promise<void> {
     const data: DepoymentDtoCreate = req.body;
     // Implement CREATE logic here
     res.status(201).send({ message: 'Depoyment created', data });
   }
 
-  static async getById(req, res) {
+  static async getById(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     // Implement GET by ID logic here
     res.status(200).send({ message: 'Fetched Depoyment by ID', id });
   }
 
-  static async getAll(req, res) {
+  static async getAll(req: Request, res: Response): Promise<void> {
     const query: DepoymentDtoFindQuery = req.query;
     // Implement GET ALL logic with query parameters
     res.status(200).send({ message: 'Fetched all Depoyments', query });
   }
 
-  static async update(req, res) {
+  static async update(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     const data: DepoymentDtoUpdate = req.body;
     // Implement PATCH logic here
     res.status(200).send({ message: 'Updated Depoyment', id, data });
   }
 
-  static async delete(req, res) {
+  static async delete(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     // Implement DELETE logic here
     res.status(200).send({ message: 'Deleted Depoyment', id });
