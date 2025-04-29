0071cfba6604a7053e95dac98c6341b0ec380aae
WallaceVidoto <wallacevidoto.tech@gmail.com>
2025-04-29 19:08:25 -0300

    build

--- backend--nodejs/src/middlewares/validation.middleware.ts
+++ backend--nodejs/src/middlewares/validation.middleware.ts
@@ -1,20 +1,21 @@
 import { ClassConstructor, plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
-import { RequestHandler } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { ResponseApi } from "../utils/response-api";
 
 export function validateDto<T>(type: ClassConstructor<T>): RequestHandler {
-  return async (req, res, next) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     const dto = plainToInstance(type, req.body);
     const errors = await validate(dto as object);
 
     if (errors.length > 0) {
-      return res.status(400).json(
+      res.status(400).json(
         ResponseApi.response({
           message: errors.map((err) => `${err.property}: ${Object.values(err.constraints || {}).join(", ")}`).join("; "),
           data: errors,
         })
       );
+      return;
     }
 
     req.body = dto;
